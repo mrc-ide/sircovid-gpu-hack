539f5f299908443a84fd40aecf4a496a2c1e339b
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-07-20 19:01:29 +0100

    More includes

--- prototype/src/sirs_gpu.hpp
+++ prototype/src/sirs_gpu.hpp
@@ -1,4 +1,6 @@
 #include <vector>
+#include "sirs_gpu.hpp"
+
 std::vector<float> sircovid_main(float alpha, float beta, float gamma, int I0,
                                  int n_particles, int n_steps, int n_record,
                                  int seed);

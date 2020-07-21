2bdf287e5b8417122aa80ab5ca9e69c1e47c3c63
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-07-21 11:10:43 +0100

    Add missing include

--- prototype/src/sirs_gpu.hpp
+++ prototype/src/sirs_gpu.hpp
@@ -1,3 +1,4 @@
+#include <cpp11/list.hpp>
 #include <vector>
 std::vector<float> sircovid_main(float alpha, float beta, float gamma, int I0,
                                  int n_particles, int n_steps, int n_record,
